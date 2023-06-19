ba3e6d9715f11c1ac7eebd6ba9694c3312b52543
gbuser <gregory.buser@gmail.com>
2023-06-19 17:52:10 -0500

    Add files via upload

---
+++ mta.py
@@ -0,0 +1,6 @@
+import mta_pb2
+import requests
+url = 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l'
+auth = 'fTrUKUpBJ35KpNIAuemqe8RjhSZdazDT6qpGUqrP'
+
+r = requests.get(url, headers = {"x-api-key": auth})
